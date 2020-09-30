031de000289bd1ff175b9f43b269bd2f44218229
Novvea <anna.c.novak@gmail.com>
2020-09-30 15:37:44 +0200

    Add kryddapp title

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Kryddapp</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -16,7 +16,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #0b4927;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
