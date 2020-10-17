438190eeb76b92d6a08a0f6c5565445a84eb2aac
Novvea <anna.c.novak@gmail.com>
2020-10-17 10:24:31 +0200

    Start work on adding spices

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,61 @@
 <script>
+	import Header from './UI/Header.svelte'
 	
+	let spiceName;
+	let spiceItems = {};
+	let buySpiceItems ={};
+	let ammount = 'empty';
+
+	function addSpice() {
+		spiceItems = {...spiceItems, [spiceName.toLowerCase()]: {ammount}}
+	}
+
+	function buySpice() {
+		if (Object.keys(spiceItems).find(() => spiceName.toLowerCase())) {
+			alert('Du har denna krydda i ditt kryddskåp')
+		} else {
+			buySpiceItems= {...buySpiceItems, [spiceName.toLowerCase()]: {ammount:'empty'}}}
+	}
+
+	function cancel () {
+		console.log('Close modal')
+	}
+
+	$: console.log(spiceItems);	//detta är ett objekt
+	//$: console.log(Object.keys(spiceList)) //detta är en array
+	$: console.log(buySpiceItems);
 </script>
 
+<Header />
+
 <main>
-	<h1>Kryddapp</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Dina kryddor:</p>
+
+	<!--Här kommer min modal-->
+	<div class="modal-backdrop"/>
+	<div class="modal">
+		<p>Fyll i din krydda här:</p>
+		<input type="text" bind:value={spiceName}>
+		<button on:click={() => (ammount='bara lite kvar')}>Bara lite kvar</button>
+		<button on:click={() => (ammount='medelmycket kvar')}>Medelmycket</button>
+		<button on:click={() => (ammount='typ full')}>Full</button>
+		<div class="content">	
+			<button on:click={addSpice}>Spara kryddan</button>
+			<button on:click={buySpice}>Köp!</button>
+		</div>
+		<footer>
+			<button on:click={cancel}>Stäng</button>
+		</footer>
+	</div>
+
+
+	<ul class="spice_list">		
+		{#each Object.keys(spiceItems) as name}
+			<li> {name} </li>
+			{:else}
+			<p>Din kryddlåda är just nu tom :(</p>
+		{/each}
+	</ul>
 </main>
 
 <style>
@@ -16,7 +67,7 @@
 	}
 
 	h1 {
-		color: #0b4927;
+		color: #cfa828;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
@@ -27,4 +78,32 @@
 			max-width: none;
 		}
 	}
+
+	.spice_list {
+		color:rgb(6, 6, 83);
+		list-style-type: none;
+	}
+
+	.modal-backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.75);
+    z-index: 10;
+  }
+
+  .modal {
+    position: fixed;
+    top: 10vh;
+    left: 10%;
+    width: 80%;
+    max-height: 80vh;
+    background: white;
+    border-radius: 5px;
+    z-index: 100;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    overflow: scroll;
+  }
 </style>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,28 @@
+<header>
+  <h1>Kryddapp</h1>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #d4d4d4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgb(0,0,0,0.26);
+  }
+
+  h1 {
+    color: #cfa828;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+    margin: 0;
+  }
+</style>
+
+
